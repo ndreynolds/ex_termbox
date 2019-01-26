a40c94115b1ca71822a5e930b3dfbdd247ce7e07
Nick Reynolds <ndreynolds@posteo.de>
2019-01-26 15:35:40 +0100

    Adds additional doc tests to ExTermbox.Position

--- lib/ex_termbox/position.ex
+++ lib/ex_termbox/position.ex
@@ -14,7 +14,7 @@ defmodule ExTermbox.Position do
   @type t :: %__MODULE__{x: non_neg_integer, y: non_neg_integer}
 
   @doc """
-  Translates (moves) a position by some delta x and y.
+  Translates (shifts) a position by some delta x and y.
 
   Returns a new `%Position{}`.
 
@@ -31,13 +31,33 @@ defmodule ExTermbox.Position do
     do: %Position{x: x + dx, y: y + dy}
 
   @doc """
-  Translate a position by a delta x.
+  Translates a position by a delta x.
+
+  Returns a new `%Position{}`.
+
+  ## Examples
+
+      iex> translate_x(%Position{x: 0, y: 0}, 2)
+      %Position{x: 2, y: 0}
+      iex> translate_x(%Position{x: 2, y: 0}, -1)
+      %Position{x: 1, y: 0}
+
   """
   @spec translate_x(t, integer) :: t
   def translate_x(%Position{} = pos, dx), do: translate(pos, dx, 0)
 
   @doc """
-  Translate a position by a delta y.
+  Translates a position by a delta y.
+
+  Returns a new `%Position{}`.
+
+  ## Examples
+
+      iex> translate_y(%Position{x: 0, y: 0}, 2)
+      %Position{x: 0, y: 2}
+      iex> translate_y(%Position{x: 0, y: 2}, -1)
+      %Position{x: 0, y: 1}
+
   """
   @spec translate_y(t, integer) :: t
   def translate_y(%Position{} = pos, dy), do: translate(pos, 0, dy)
